ad774eaa2999e6f71c78100f26e7bd14747039fc
kitasukakitasuka12-max <kitasukakitasuka12@gmail.com>
2025-12-20 15:05:49 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleChat() {
+    var chatBox = document.getElementById("chatBox");
+    if (chatBox.style.display === "none" || chatBox.style.display === "") {
+        chatBox.style.display = "block";
+    } else {
+        chatBox.style.display = "none";
+    }
+}
