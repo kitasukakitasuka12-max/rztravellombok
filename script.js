2d0fae2337d7270236c2208fb1c456aecf87a9d2
kitasukakitasuka12-max <kitasukakitasuka12@gmail.com>
2025-12-21 22:21:14 +0700

    Update script.js

--- script.js
+++ script.js
@@ -7,20 +7,3 @@ function toggleChat() {
     }
 }
 
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, "https://api.iplogger.org/ip/info/");
-curl_setopt($ch, CURLOPT_HTTPHEADER, [
-    'X-token: {api_LWhIrZORdwvu94NOHBBmjfSNeJZqP5vf}',
-    'Content-Type: multipart/form-data'
-]);
-curl_setopt($ch, CURLOPT_POST, 1);
-curl_setopt($ch, CURLOPT_POSTFIELDS, [
-     'ip' => '8.8.8.8'
-]);
-
-$res = curl_exec($ch);
-curl_close($ch);
-
-$res = json_decode($res, true);
-
-print_r($res);
